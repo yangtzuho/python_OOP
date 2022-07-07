5af9856f521d22761d738df4f43633c8a382136c
yangzh <yzh_1008@163.com>
2022-07-07 22:56:05 +0800

    __repr__

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    def __repr__(self) -> str:
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
+
 
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
@@ -28,7 +31,6 @@ item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
 
 print(Item.all)
-for instance in Item.all:
-    print(instance.name)
+
 
 
