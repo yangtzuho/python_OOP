241fbe76bd892bfa32dc9ec035b574cdaa61984c
yangzh <yzh_1008@163.com>
2022-07-07 22:18:21 +0800

    assert

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import queue
+
+
 class Item:
-    def __init__(self, name, price, quantity) -> None:
+    def __init__(self, name:  str, price: float, quantity = 0) -> None:
+        # Rum validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quentity {quantity} is not greater than zero!"
+        # Assign to self object
         self.name  = name
         self.price = price 
         self.quantity = quantity
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item("Phone", 100, 5)
+item1 = Item("Phone", 100, -1)
 
 item2 = Item("Laptop", 1000, 3)
 
-print(item1.name, item2.name)
-print(item1.price, item2.price)
-print(item1.quantity, item2.quantity)
+print(item1.calculate_total_price(), item2.calculate_total_price())
+
