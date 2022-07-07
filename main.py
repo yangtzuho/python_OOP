2b360031b2df48929e74d99164ff84ac4fec8bb5
yangzh <yzh_1008@163.com>
2022-07-07 22:49:41 +0800

    Item.all

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 class Item:
     pay_rate = 0.8 # The pay rate 
+    all = []
     def __init__(self, name:  str, price: float, quantity = 0) -> None:
         # Rum validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than zero!"
         assert quantity >= 0, f"Quentity {quantity} is not greater than zero!"
+
         # Assign to self object
         self.name  = name
         self.price = price 
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
 
 
 item1 = Item("Phone", 100, 1)
-
 item2 = Item("Laptop", 1000, 3)
-item2.apply_discount()
-print(item2.price)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-#print(item1.calculate_total_price(), item2.calculate_total_price())
-#print(Item.pay_rate)
+print(Item.all)
+for instance in Item.all:
+    print(instance.name)
 
-print(Item.__dict__) # All the attributes for Class level
-print(item1.__dict__) # All the attributes for instance level
+
