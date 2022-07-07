032c8e71b943f3649707ca4c56a7bf73f3cafb69
yangzh <yzh_1008@163.com>
2022-07-07 21:21:43 +0800

    constructor

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Item:
-    def __init__(self, name) -> None:
+    def __init__(self, name, price, quantity) -> None:
         self.name  = name
+        self.price = price 
+        self.quantity = quantity
     def calculate_total_price(self, x, y):
         return x * y
 
 
-item1 = Item("Phone")
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone", 100, 5)
 
-item2 = Item("Laptop")
-item2.price = 1000
-item2.quantity = 3
+item2 = Item("Laptop", 1000, 3)
 
 print(item1.name, item2.name)
+print(item1.price, item2.price)
+print(item1.quantity, item2.quantity)
