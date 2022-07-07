78d7237518f57edd227cc28c2735f5221d7cfa05
yangzh <yzh_1008@163.com>
2022-07-07 21:06:34 +0800

    first create a method for this class

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 class Item:
-    pass
+    def calculate_total_price(self, x, y):
+        return x * y
 
 
 item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
 
-print(type(item1))
-print(type(item1.name))
-print(type(item1.price))
-print(type(item1.quantity))
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
+print(item2.calculate_total_price(item2.price, item2.quantity)) 
